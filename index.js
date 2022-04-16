6c796e7583b3fc57189ef7747613f998595c95fc
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-04-16 14:29:25 +0100

    included static hosting

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());    // add JSON express middleware
-
+app.use(express.json());    // adds JSON express middleware
+app.use(express.static('/static'));   // adds static hosting for 'static' folder
 let todos = [];   // handles all the todos
+
 
 /*
   -------------------- get all todos -------------------------
